2350b1fa8dc487db426ccb9633f11092faf1ec2f
HVMarci <horvath.varga.marton@gmail.com>
2021-12-24 10:27:46 +0100

    Kész a BOLDOG KARÁCSONYT felirat, de még nagyon hülény néz ki (BOLDOGKA       RÁCSONYT)

--- WithSprites.asm
+++ WithSprites.asm
@@ -120,28 +120,31 @@ endrepeat
 ; V2
         sta $07F8 ; 4 cycles
 
-        adc #2 ; 2 cycles
+        adc #4 ; 2 cycles
         sta $07F9 ; 4 cycles
 
-        adc #2
+        adc #4
         sta $07FA
-        adc #2
+        adc #4
         sta $07FB
-        adc #2
+        adc #4
         sta $07FC
-        adc #2
+        adc #4
         sta $07FD
-        adc #2
+        adc #4
         sta $07FE
-        adc #2
+        adc #4
         sta $07FF
         ; total: 4 + 7 * 6 = 46 cycles => éppen belefér az egy sornyi 47-be
         ; egész total: 168 ciklus (TODO: rájönni hogy mi a francért kell ennyit várni)
 
         and #$01
-        bne @Finish ; csak egyszer változtassuk az X-et
+        beq @Next ; csak egyszer változtassuk az X-et
+        jmp @Finish
 
-        lda #$00
+
+; az X pozíciók 8. bitjének megváltoztatása + betűváltás
+@Next   lda #$00
 @S0     ldx $D000
         bne @S1
         ora #1
@@ -174,6 +177,27 @@ endrepeat
         bne @SE
         ora #128
 
+        tay
+
+        lda $07F8
+        eor #%00000010
+        sta $07F8
+        adc #4
+        sta $07F9
+        adc #4
+        sta $07FA
+        adc #4
+        sta $07FB
+        adc #4
+        sta $07FC
+        adc #4
+        sta $07FD
+        adc #4
+        sta $07FE
+        adc #4
+
+        tya
+
 @SE     eor $D010 ; 8th bit of X positions
         sta $D010
 
@@ -204,38 +228,36 @@ endrepeat
 
 
 SetupSprites
-        lda #$01 ; white
-        sta $D027 ; color of sprite 0
-        sta $D028 ; color of sprite 1
-        sta $D029 ; color of sprite 2
-        sta $D02A ; color of sprite 3
-        sta $D02B ; color of sprite 4
-        sta $D02C ; color of sprite 5
-        lda #$00 ; black
-        sta $D02D ; color of sprite 6
-        sta $D02E ; color of sprite 7
+        lda #$08
+        ldx #$08
+        sec
+@Loop1  sta $D026
+        sbc #$01
+        dex
+        bne @Loop1
 
         lda #$00
         sta $D01C ; multicolor mode off
         lda #%11111111
         sta $D015 ; turn the sprites on
 
-        lda #240
-        sta $07F8 ; set sprite 0's pointer to $3C00-$3C3E
-        lda #242
-        sta $07F9 ; set sprite 1's pointer to $3C80-$3CBE
-        lda #244
-        sta $07FA ; set sprite 2's pointer to $3D00-$3D3E
-        lda #246
-        sta $07FB ; set sprite 3's pointer to $3D80-$3DBE
-        lda #248
-        sta $07FC ; set sprite 4's pointer to $3E00-$3E3E
-        lda #250
-        sta $07FD ; set sprite 5's pointer to $3E80-$3EBE
-        lda #252
-        sta $07FE ; set sprite 6's pointer to $3F00-$3F3E
-        lda #254
-        sta $07FF ; set sprite 7's pointer to $3F80-$3FBE
+        ; set sprites' pointers
+        lda #200
+        sta $07F8
+        lda #204
+        sta $07F9
+        lda #208
+        sta $07FA
+        lda #212
+        sta $07FB
+        lda #216
+        sta $07FC
+        lda #220
+        sta $07FD
+        lda #224
+        sta $07FE
+        lda #228
+        sta $07FF
 
         lda #$00
         sta $D01D ; sprite nagyítás X irányban
@@ -288,42 +310,80 @@ SetupSprites
         ; copy sprite datas into memory
         ldx #63
 @Loop   lda SpriteBTeteje,X
-        sta $3C00,X
+        sta $3200,X
         lda SpriteBAlja,X
-        sta $3C40,X
+        sta $3240,X
+        lda SpriteRTeteje,X
+        sta $3280,X
+        lda SpriteRAlja,X
+        sta $32C0,X
 
         lda SpriteOTeteje,X
-        sta $3C80,X
-        sta $3E00,X
+        sta $3300,X
         lda SpriteOAlja,X
-        sta $3CC0,X
-        sta $3E40,X
+        sta $3340,X
+        lda SpriteAATeteje,X
+        sta $3380,X
+        lda SpriteAAAlja,X
+        sta $33C0,X
 
         lda SpriteLTeteje,X
-        sta $3D00,X
+        sta $3400,X
         lda SpriteLAlja,X
-        sta $3D40,X
+        sta $3440,X
+        lda SpriteCTeteje,X
+        sta $3480,X
+        lda SpriteCAlja,X
+        sta $34C0,X
 
         lda SpriteDTeteje,X
-        sta $3D80,X
+        sta $3500,X
         lda SpriteDAlja,X
-        sta $3DC0,X
+        sta $3540,X
+        lda SpriteSTeteje,X
+        sta $3580,X
+        lda SpriteSAlja,X
+        sta $35C0,X
+
+        lda SpriteOTeteje,X
+        sta $3600,X
+        sta $3680,X
+        lda SpriteOAlja,X
+        sta $3640,X
+        sta $36C0,X
 
         lda SpriteGTeteje,X
-        sta $3E80,X
+        sta $3700,X
         lda SpriteGAlja,X
-        sta $3EC0,X
+        sta $3740,X
+        lda SpriteNTeteje,X
+        sta $3780,X
+        lda SpriteNAlja,X
+        sta $37C0,X
 
-        lda Sprite1,X
-        sta $3F00,X
-        sta $3F80,X
-        lda Sprite2,X
-        sta $3F40,X
-        sta $3FC0,X
+        lda SpriteKTeteje,X
+        sta $3800,X
+        lda SpriteKAlja,X
+        sta $3840,X
+        lda SpriteYTeteje,X
+        sta $3880,X
+        lda SpriteYAlja,X
+        sta $38C0,X
+
+        lda SpriteATeteje,X
+        sta $3900,X
+        lda SpriteAAlja,X
+        sta $3940,X
+        lda SpriteTTeteje,X
+        sta $3980,X
+        lda SpriteTAlja,X
+        sta $39C0,X
+
         dex
-        bpl @Loop ; end at X=255
+        bmi @End ; end at X=255
+        jmp @Loop
 
-        rts
+@End    rts
 
 
 
@@ -723,7 +783,327 @@ SpriteAAlja
         BYTE $80,$00,$01
         BYTE $80,$00,$01
 
+SpriteRTeteje
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $3F,$C0,$00
+        BYTE $3F,$F8,$00
+        BYTE $3C,$FF,$00
+        BYTE $3C,$0F,$C0
+        BYTE $3C,$01,$E0
+        BYTE $3C,$00,$F0
+        BYTE $3C,$00,$78
+        BYTE $3C,$00,$38
+        BYTE $3C,$00,$1C
+        BYTE $3C,$00,$0C
+        BYTE $3C,$00,$06
+        BYTE $3C,$00,$06
+        BYTE $3C,$00,$06
+        BYTE $3C,$00,$06
+        BYTE $3C,$00,$06
+        BYTE $3C,$00,$06
+        BYTE $3C,$00,$0C
+        BYTE $3C,$00,$1C
+        BYTE $3C,$00,$38
 
+SpriteRAlja
+        BYTE $3C,$00,$38
+        BYTE $3C,$00,$F8
+        BYTE $3C,$0F,$F8
+        BYTE $3F,$FF,$F0
+        BYTE $3F,$FF,$80
+        BYTE $3F,$FC,$00
+        BYTE $3C,$1C,$00
+        BYTE $3C,$0E,$00
+        BYTE $3C,$0F,$00
+        BYTE $3C,$07,$00
+        BYTE $3C,$07,$80
+        BYTE $3C,$03,$80
+        BYTE $3C,$01,$C0
+        BYTE $3C,$01,$E0
+        BYTE $3C,$00,$E0
+        BYTE $3C,$00,$70
+        BYTE $3C,$00,$70
+        BYTE $3C,$00,$78
+        BYTE $3C,$00,$38
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+
+SpriteAATeteje ; á
+        BYTE $00,$00,$60
+        BYTE $00,$00,$E0
+        BYTE $00,$00,$C0
+        BYTE $00,$01,$C0
+        BYTE $00,$1B,$80
+        BYTE $00,$1B,$80
+        BYTE $00,$3D,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$66,$00
+        BYTE $00,$66,$00
+        BYTE $00,$E7,$00
+        BYTE $00,$C3,$00
+        BYTE $00,$C3,$00
+        BYTE $00,$C3,$00
+        BYTE $01,$C3,$80
+        BYTE $01,$81,$80
+        BYTE $01,$81,$80
+        BYTE $03,$81,$C0
+        BYTE $03,$00,$C0
+        BYTE $03,$00,$C0
+
+SpriteAAAlja ; á
+        BYTE $07,$00,$E0
+        BYTE $07,$00,$E0
+        BYTE $07,$00,$E0
+        BYTE $07,$FF,$E0
+        BYTE $07,$FF,$E0
+        BYTE $0F,$FF,$F0
+        BYTE $0E,$00,$70
+        BYTE $0E,$00,$70
+        BYTE $0C,$00,$38
+        BYTE $1C,$00,$38
+        BYTE $1C,$00,$38
+        BYTE $1C,$00,$38
+        BYTE $3C,$00,$3C
+        BYTE $38,$00,$1C
+        BYTE $38,$00,$1C
+        BYTE $38,$00,$1C
+        BYTE $30,$00,$0C
+        BYTE $30,$00,$0C
+        BYTE $30,$00,$0C
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+
+SpriteCTeteje
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $00,$1F,$C0
+        BYTE $00,$3F,$F0
+        BYTE $00,$70,$78
+        BYTE $00,$E0,$38
+        BYTE $01,$C0,$1C
+        BYTE $01,$C0,$0C
+        BYTE $03,$80,$06
+        BYTE $03,$00,$06
+        BYTE $07,$00,$02
+        BYTE $06,$00,$00
+        BYTE $0E,$00,$00
+        BYTE $0E,$00,$00
+        BYTE $0C,$00,$00
+        BYTE $0C,$00,$00
+        BYTE $0C,$00,$00
+        BYTE $0C,$00,$00
+        BYTE $1C,$00,$00
+        BYTE $1C,$00,$00
+
+SpriteCAlja
+        BYTE $1C,$00,$00
+        BYTE $1C,$00,$00
+        BYTE $1C,$00,$00
+        BYTE $1C,$00,$00
+        BYTE $1C,$00,$00
+        BYTE $0C,$00,$00
+        BYTE $0E,$00,$00
+        BYTE $0E,$00,$00
+        BYTE $0E,$00,$00
+        BYTE $06,$00,$0E
+        BYTE $07,$00,$0E
+        BYTE $07,$80,$0E
+        BYTE $03,$80,$1E
+        BYTE $01,$C0,$1C
+        BYTE $01,$E0,$FC
+        BYTE $00,$FF,$F8
+        BYTE $00,$7F,$E0
+        BYTE $00,$3F,$80
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+
+SpriteSTeteje
+        BYTE $00,$00,$00
+        BYTE $00,$3C,$00
+        BYTE $01,$FF,$C0
+        BYTE $07,$FF,$E0
+        BYTE $07,$81,$F0
+        BYTE $0F,$00,$F8
+        BYTE $0E,$00,$78
+        BYTE $0E,$00,$38
+        BYTE $0E,$00,$00
+        BYTE $1C,$00,$00
+        BYTE $1C,$00,$00
+        BYTE $1C,$00,$00
+        BYTE $0E,$00,$00
+        BYTE $0E,$00,$00
+        BYTE $0E,$00,$00
+        BYTE $07,$00,$00
+        BYTE $07,$00,$00
+        BYTE $03,$80,$00
+        BYTE $01,$C0,$00
+        BYTE $00,$F8,$00
+        BYTE $00,$3C,$00
+
+SpriteSAlja
+        BYTE $00,$3C,$00
+        BYTE $00,$1F,$80
+        BYTE $00,$0F,$E0
+        BYTE $00,$01,$F0
+        BYTE $00,$00,$F0
+        BYTE $00,$00,$F0
+        BYTE $00,$00,$78
+        BYTE $00,$00,$38
+        BYTE $00,$00,$1C
+        BYTE $00,$00,$1C
+        BYTE $00,$00,$1C
+        BYTE $00,$00,$0C
+        BYTE $30,$00,$0C
+        BYTE $38,$00,$0C
+        BYTE $38,$00,$1C
+        BYTE $1C,$00,$7C
+        BYTE $1E,$00,$F8
+        BYTE $0F,$FF,$E0
+        BYTE $07,$FF,$80
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+
+SpriteNTeteje
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $3E,$00,$3C
+        BYTE $3E,$00,$3C
+        BYTE $3F,$00,$3C
+        BYTE $3F,$80,$3C
+        BYTE $3F,$80,$3C
+        BYTE $3F,$C0,$3C
+        BYTE $3F,$C0,$3C
+        BYTE $3F,$C0,$3C
+        BYTE $3F,$E0,$3C
+        BYTE $3D,$E0,$3C
+        BYTE $3C,$E0,$3C
+        BYTE $3C,$F0,$3C
+        BYTE $3C,$70,$3C
+        BYTE $3C,$70,$3C
+        BYTE $3C,$78,$3C
+        BYTE $3C,$78,$3C
+        BYTE $3C,$38,$3C
+        BYTE $3C,$3C,$3C
+        BYTE $3C,$3C,$3C
+
+SpriteNAlja
+        BYTE $3C,$3C,$3C
+        BYTE $3C,$3C,$3C
+        BYTE $3C,$1E,$3C
+        BYTE $3C,$1E,$3C
+        BYTE $3C,$0E,$3C
+        BYTE $3C,$0F,$3C
+        BYTE $3C,$0F,$3C
+        BYTE $3C,$07,$BC
+        BYTE $3C,$07,$BC
+        BYTE $3C,$03,$FC
+        BYTE $3C,$01,$FC
+        BYTE $3C,$01,$FC
+        BYTE $3C,$01,$FC
+        BYTE $3C,$00,$FC
+        BYTE $3C,$00,$FC
+        BYTE $3C,$00,$7C
+        BYTE $3C,$00,$3C
+        BYTE $3C,$00,$3C
+        BYTE $3C,$00,$3C
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+
+SpriteYTeteje
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $30,$00,$0C
+        BYTE $38,$00,$1C
+        BYTE $1C,$00,$38
+        BYTE $1C,$00,$38
+        BYTE $0E,$00,$70
+        BYTE $0F,$00,$F0
+        BYTE $07,$00,$E0
+        BYTE $07,$81,$E0
+        BYTE $03,$81,$C0
+        BYTE $01,$C3,$80
+        BYTE $01,$E7,$80
+        BYTE $00,$FF,$00
+        BYTE $00,$FF,$00
+        BYTE $00,$7E,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+
+SpriteYAlja
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+
+SpriteTTeteje
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $3F,$FF,$FC
+        BYTE $3F,$FF,$FC
+        BYTE $3F,$FF,$FC
+        BYTE $3F,$FF,$FC
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+
+SpriteTAlja
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$3C,$00
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
 
 
 
